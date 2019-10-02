57c91cf42a0a446a93226ef767b87cd224114fdd
Dewald Els <dewald.els@noroff.no>
2019-10-02 17:34:42 +0200

    Removed login details for DB

--- models/db.model.js
+++ models/db.model.js
@@ -4,11 +4,11 @@ class Database {
     
     constructor() {
         this.pool = new Pool({
-            database: 'd6k5824gm3684r',
-            host: 'ec2-54-204-37-92.compute-1.amazonaws.com',
+            database: '<your-db>',
+            host: '<your-host>',
             port: 5432,
-            user: 'bqtrrikblhhgap',
-            password: '75d501bbe0426538c964adb6c683b59213f509d0781a1ecd6fe5ef251e06ff7c'
+            user: '<your-user>',
+            password: '<your-password>'
         });
     }
     
